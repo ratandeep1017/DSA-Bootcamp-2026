8649221b0f15179e77e4266eca46a45cc044ccdd
ratandeep1017 <ratandeep1017@gmail.com>
2026-05-28 16:33:23 +0530

    edit

---
+++ Arrays_Java/find_large_small_ele.java
@@ -0,0 +1,15 @@
+package Arrays_Java;
+public class find_large_small_ele {
+    public static void main(String[] args){
+        int arr[] = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        int max = arr[0], min = arr[0];
+        
+        for(int i = 1; i < n; i++){  
+            if(max < arr[i]) max = arr[i];   
+            if(min > arr[i]) min = arr[i];   
+        }
+        
+        System.out.println("Max : " + max + " Min : " + min);
+    }
+}
