9800461e54023773943b683037ba4cfabc7c1a34
Ratandeep Singh <125744520+ratandeep1017@users.noreply.github.com>
2026-07-18 22:37:46 +0530

    Update CountElements.java

--- 00_ArrayList_Java_Assessment_ques/CountElements.java
+++ 00_ArrayList_Java_Assessment_ques/CountElements.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 class hello{
     public static void main(String[] args){
         ArrayList<Integer> nums = new ArrayList<>();
